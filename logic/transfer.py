48a1d9d8125a5b2203b17026a35ce1730e4ff00f
carolinenotfound <pjerogi69@gmail.com>
2025-10-22 19:03:07 +0200

    Add files via upload

--- logic/transfer.py
+++ logic/transfer.py
@@ -1,19 +1,19 @@
 def calculate_transfer(amnt):
- if amnt < 5000:
-        return 0, amnt, 0
- elif 5001 <= amnt <= 10000:
+    if amnt <= 5000:
+        return 0, 0, 0  # No tax applies
+    elif 5000 < amnt <= 10000:
         tax = 10
- elif 10001 <= amnt <= 25000:
+    elif 10000 < amnt <= 25000:
         tax = 15
- elif 25001 <= amnt <= 35000:
+    elif 25000 < amnt <= 35000:
         tax = 25
- elif 35001 <= amnt <= 50000:
+    elif 35000 < amnt <= 50000:
         tax = 30
- elif amnt >= 50001:
+    elif amnt > 50000:
         tax = 40
- else:
-        return None, None, None  # invalid
+    else:
+        return None, None, None  # invalid input
 
- amnta = int(amnt * tax / 100)
- amntb = int(amnt - amnta)
- return amnta, amntb, tax
+    amnta = int(amnt * tax / 100)
+    amntb = int(amnt - amnta)
+    return amnta, amntb, tax
