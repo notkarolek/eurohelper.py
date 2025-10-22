84c81a18722b3cf3e31dd058ca4de468e28b686d
carolinenotfound <pjerogi69@gmail.com>
2025-10-22 20:55:02 +0200

    Add files via upload

--- logic/cartax.py
+++ logic/cartax.py
@@ -1,27 +1,21 @@
-
 def calculate_cartax(amnt):
- if  amnt <  15000:
-    print("No tax!")
- elif amnt <= 25000 and amnt >= 15000:
-    tax = 15
-    amnta = (amnt * tax / 100)
- elif amnt <= 50000 and amnt >= 25000:
-    tax = 20
-    amnta = (amnt * tax / 100)
- elif amnt <= 80000 and amnt >= 50000:
-    tax = 30
-    amnta = (amnt * tax / 100)
- elif amnt <= 100000 and amnt >= 80000:
-    tax = 40
-    amnta = (amnt * tax / 100) 
- elif amnt > 100000 and amnt <= 250001:
-    tax = 55
-    amnta = (amnt * tax / 100)
- elif amnt > 250000:
+    if amnt < 15000:
+        return 0, amnt, 0  # no tax applies
+    elif 15000 <= amnt <= 25000:
+        tax = 15
+    elif 25001 <= amnt <= 50000:
+        tax = 20
+    elif 50001 <= amnt <= 80000:
+        tax = 30
+    elif 80001 <= amnt <= 100000:
+        tax = 40
+    elif 100001 <= amnt <= 250000:
+        tax = 55
+    elif amnt > 250000:
         return None, None, None  # too much
- else:
+    else:
         return None, None, None  # invalid input
 
- amnta = int(amnt * tax / 100)
- amntb = int(amnt - amnta)
- return amnta, amntb, tax
+    amnta = int(amnt * tax / 100)
+    amntb = int(amnt - amnta)
+    return amnta, amntb, tax
